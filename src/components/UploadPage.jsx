a2eb74846b18c58913766eb1358b8bda737a34ea
unknown <Buzz-brain>
2024-12-02 23:23:00 +0100

    first commit

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -32,7 +32,7 @@ const UploadForm = () => {
     }
 
     try {
-      const response = await axios.post('https://dashmeafrica.onrender.com/api/products', formData, {
+      const response = await axios.post('https://dashmeafrica-backend.onrender.com/api/products', formData, {
       // const response = await axios.post('http://localhost:5000/api/products', formData, {
         headers: {
           'Content-Type': 'application/json',
