c6075a8af107b3ce1ab2c376c6242ad567540bce
unknown <Buzz-brain>
2024-12-03 11:27:39 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,47 +5,73 @@ import { FaUser, FaBell, FaEnvelope, FaHeart, FaSearch } from 'react-icons/fa';
 const Header = () => {
   return (
     <header className="bg-white shadow-sm">
-      <Navbar bg="white" variant="light" expand="lg" collapseOnSelect>
-        <Container>
-          {/* Logo / Brand */}
+      <Navbar bg="white" expand="lg" className="px-3">
+        <Container fluid>
+          {/* Logo */}
           <Navbar.Brand href="/" className="d-flex align-items-center">
-            <img src="https://res.cloudinary.com/df2q6gyuq/image/upload/v1733152278/dashme-logo_asszym.png" alt="Logo" height="30" /> {/* Replace with your logo image */}
+            <span
+              className="fw-bold text-success"
+              style={{ fontSize: '1.5rem', fontFamily: 'Arial' }}
+            >
+              db
+            </span>
           </Navbar.Brand>
-          {/* Search Bar */}
-          <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-center">
-            <Form className="d-flex" style={{ width: '50%', border: "1px solid green", borderRadius: '25px' }}>
+
+          {/* Toggle for Mobile */}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* Search Bar */}
+            <Form className="d-flex mx-auto my-3 my-lg-0" style={{ maxWidth: '600px', width: '100%' }}>
               <FormControl
                 type="search"
                 placeholder="Search for items"
-                className="me-2"
+                className="rounded-start"
                 aria-label="Search"
-                style={{ borderBottomLeftRadius: '25px', borderTopLeftRadius: '25px'  }}
+                style={{
+                  border: '1px solid green',
+                  borderRight: 'none',
+                  borderTopLeftRadius: '25px',
+                  borderBottomLeftRadius: '25px',
+                }}
               />
-              <Button variant="outline-success" style={{ borderRadius: '25px' }}>
-                <FaSearch /> {/* Search icon */}
+              <Button
+                variant="outline-success"
+                className="rounded-end"
+                style={{
+                  backgroundColor: 'green',
+                  color: 'white',
+                  borderTopRightRadius: '25px',
+                  borderBottomRightRadius: '25px',
+                  border: '1px solid green',
+                }}
+              >
+                <FaSearch />
               </Button>
             </Form>
-          </Navbar.Collapse>
-
-          {/* Navbar links */}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="flex">
-              <Nav.Link href="/messages">
-                <FaEnvelope />
+          {/* Navbar Links */}
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
+            {/* Icons and Links */}
+            <Nav className="d-flex align-items-center">
+              <Nav.Link href="/messages" className="me-2">
+                <FaEnvelope size={18} />
               </Nav.Link>
-              <Nav.Link href="/notifications">
-                <FaBell />
+              <Nav.Link href="/notifications" className="me-2">
+                <FaBell size={18} />
               </Nav.Link>
-              <Nav.Link href="/favorites">
-                <FaHeart />
+              <Nav.Link href="/favorites" className="me-2">
+                <FaHeart size={18} />
               </Nav.Link>
-              <Nav.Link href="/account">
-                <FaUser />
+              <Nav.Link href="/account" className="me-3">
+                <FaUser size={18} />
               </Nav.Link>
-              <Nav.Link href="/upload">Upload</Nav.Link>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-              <Nav.Link href="/language">EN</Nav.Link>
+              <Nav.Link href="/upload" className="me-3 fw-bold text-dark">
+                Upload
+              </Nav.Link>
+              <Nav.Link href="/signup" className="me-3 fw-bold text-dark">
+                Sign Up
+              </Nav.Link>
+              <Nav.Link href="/language" className="fw-bold text-dark">
+                EN <span className="fw-light">&#x25BC;</span>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -53,4 +79,5 @@ const Header = () => {
     </header>
   );
 };
+
 export default Header;
