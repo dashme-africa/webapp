c44b0a33bbe2672745c02c2d80b2ca6ab6647cfa
unknown <Buzz-brain>
2025-01-13 20:07:29 +0100

    Config to deploy on render

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures paths are resolved correctly
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Optional: Needed for local development with SPA routing
+  },
+  build: {
+    outDir: 'dist', // Render expects the build files in the "dist" folder
+    emptyOutDir: true, // Clears old build files before creating new ones
   },
 });
-
